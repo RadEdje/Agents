91b4398dafc6ffb1366cb78461184186d97ccfe9
Erwin_home <ejtcarpio@gmail.com>
2025-06-22 22:25:15 +0800

    server error with mcp port

--- adk_api.py
+++ adk_api.py
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        # port=int(os.environ.get("PORT", 8000)) #comment this out if on local pc to protect your ports
+        port=int(os.environ.get("PORT", 8000)) #comment this out if on local pc to protect your ports
     )
